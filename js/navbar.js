ab0ac5b0ae8d2345135b600d542eb8d4f72db4f9
markcc77 <70893134+markcc77@users.noreply.github.com>
2022-04-07 01:04:25 +0800

    aa

--- js/navbar.js
+++ js/navbar.js
@@ -3,22 +3,13 @@ window.addEventListener('DOMContentLoaded', event => {
     // Navbar shrink function
     var navbarShrink = function () {
         const navbarCollapsible = document.body.querySelector('#mainNav');
-        const navbarCollapsibleText = document.body.querySelector('#mainNavText');
-        const navbarCollapsibleLogo = document.body.querySelector('#mainNavLogo');
-        const navbarCollapsibleDate = document.body.querySelector('#mainNavDate');
         if (!navbarCollapsible) {
             return;
         }
         if (window.scrollY === 0) {
             navbarCollapsible.classList.remove('navbar-shrink')
-            navbarCollapsibleText.style.display = 'none'
-            navbarCollapsibleLogo.style.display = 'none'
-            navbarCollapsibleDate.innerHTML = ''
         } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-            navbarCollapsibleText.style.display = ''
-            navbarCollapsibleLogo.style.display = 'inline-block'
-            navbarCollapsibleDate.innerHTML = datehax()
+            navbarCollapsible.classList.add('navbar-shrink') 
         }
 
     };
